439ded9833234884506cd5e4ffbf9e16440f6222
Douglas Basilio <basilio.douglas@yahoo.com.br>
2021-11-01 13:12:51 -0300

    Add js folder and function js

---
+++ assets/js/app.js
@@ -0,0 +1,29 @@
+/*Whatsapp*/
+
+function hoverWhats() {
+    document.getElementById("whatsapp-icon").src = "assets/icons/Whatsapp-1.png";
+}
+
+function mouseOutWhats() {
+    document.getElementById("whatsapp-icon").src = "assets/icons/whatsapp.png";
+}
+
+/*Instagram*/
+
+function hoverInsta() {
+    document.getElementById("insta-icon").src = "assets/icons/Instagram-1.png";
+}
+
+function mouseOutInsta() {
+    document.getElementById("insta-icon").src = "assets/icons/Instagram.png";
+}
+
+/*Linkedin*/
+
+function hoverLinkedin() {
+    document.getElementById("linkedin-icon").src = "assets/icons/LinkedIn-1.png";
+}
+
+function mouseOutLinkedin() {
+    document.getElementById("linkedin-icon").src = "assets/icons/LinkedIn.png";
+}
