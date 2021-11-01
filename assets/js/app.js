c900d7358a714b8a4d54d99139e2dad2739326a6
Douglas Basilio <basilio.douglas@yahoo.com.br>
2021-11-01 20:33:48 -0300

    Alteração das imagens para o formato webp

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,29 +1,29 @@
 /*Whatsapp*/
 
 function hoverWhats() {
-    document.getElementById("whatsapp-icon").src = "assets/icons/Whatsapp-1.png";
+    document.getElementById("whatsapp-icon").src = "assets/icons/Whatsapp-1.webp";
 }
 
 function mouseOutWhats() {
-    document.getElementById("whatsapp-icon").src = "assets/icons/whatsapp.png";
+    document.getElementById("whatsapp-icon").src = "assets/icons/whatsapp.webp";
 }
 
 /*Instagram*/
 
 function hoverInsta() {
-    document.getElementById("insta-icon").src = "assets/icons/Instagram-1.png";
+    document.getElementById("insta-icon").src = "assets/icons/Instagram-1.webp";
 }
 
 function mouseOutInsta() {
-    document.getElementById("insta-icon").src = "assets/icons/Instagram.png";
+    document.getElementById("insta-icon").src = "assets/icons/Instagram.webp";
 }
 
 /*Linkedin*/
 
 function hoverLinkedin() {
-    document.getElementById("linkedin-icon").src = "assets/icons/LinkedIn-1.png";
+    document.getElementById("linkedin-icon").src = "assets/icons/LinkedIn-1.webp";
 }
 
 function mouseOutLinkedin() {
-    document.getElementById("linkedin-icon").src = "assets/icons/LinkedIn.png";
+    document.getElementById("linkedin-icon").src = "assets/icons/LinkedIn.webp";
 }
